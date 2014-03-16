5079d143b6bf369a599b7498410d50113f773e7d
洪盟翔 <andy@eztable.com.tw>
2014-03-17 00:08:23 +0800

    generate fruit

--- Main.hs
+++ Main.hs
@@ -6,6 +6,12 @@ main :: IO ()
 main = do 
     let game = Game {
         snake = Snake RIGHT [Point 3 5, Point 3 4, Point 3 3, Point 3 2],
-        board = Board 30 30 
+        board = Board 30 30 ,
+        status = Running,
+        fruit = Point 0 0
     }
-    gameLoop game
+    f <- genFruit game
+
+    let withFruit = game {fruit = f}
+
+    gameLoop withFruit
--- Snake.hs
+++ Snake.hs
@@ -2,6 +2,8 @@ module Snake
 where
 
 import UI.NCurses as NC
+import Data.List
+import System.Random
 
 data Direction = LEFT | RIGHT | UP | DOWN deriving (Eq, Show)
 data Point = Point Int Int deriving (Eq, Show)
@@ -24,12 +26,15 @@ instance Drawable Point where
     draw (Point x y) = putPoint (toInteger x) (toInteger y) $ "*"
 
 instance Drawable Board where
-    draw (Board w h) = do
-        mapM_ draw points
-        where points = [Point 0 y | y <- [0..w-1]] ++ 
-                       [Point x 0 | x <- [0..h-1]] ++ 
-                       [Point (h-1) y | y <- [0..w-1]] ++ 
-                       [Point x (w-1) | x <- [0..h-1]]
+    draw b = do
+        mapM_ draw $ getBoardPoints b
+
+getBoardPoints :: Board -> [Point]
+getBoardPoints (Board w h) = 
+    [Point 0 y | y <- [0..w-1]] ++ 
+    [Point x 0 | x <- [0..h-1]] ++ 
+    [Point (h-1) y | y <- [0..w-1]] ++ 
+    [Point x (w-1) | x <- [0..h-1]]
 
 instance Drawable Snake where
     draw (Snake _ points) = mapM_ draw points
@@ -37,11 +42,11 @@ instance Drawable Snake where
 advance :: Direction -> Snake -> Snake
 advance dir s@(Snake origDir points) 
     | isAllow dir origDir = case dir of
-        UP -> Snake UP $ (Point (hx-1) hy):(init points)
-        DOWN -> Snake DOWN $ (Point (hx+1) hy):(init points)
-        LEFT -> Snake LEFT $ (Point hx (hy-1)):(init points)
+        UP    -> Snake UP $ (Point (hx-1) hy):(init points)
+        DOWN  -> Snake DOWN $ (Point (hx+1) hy):(init points)
+        LEFT  -> Snake LEFT $ (Point hx (hy-1)):(init points)
         RIGHT -> Snake RIGHT $ (Point hx (hy+1)):(init points)
-    | otherwise = advance dir s
+    | otherwise = advance origDir s 
     where 
         (Point hx hy) = head points
         isAllow UP DOWN = False
@@ -50,21 +55,37 @@ advance dir s@(Snake origDir points)
         isAllow LEFT RIGHT = False
         isAllow _ _ = True
 
+data Status = HitFruit | Loose | Running
+
+type Fruit = Point
+
 data Game = Game {
     snake :: Snake,
-    board :: Board
+    board :: Board,
+    status :: Status,
+    fruit :: Fruit
 }
+
+getIdlePoints:: Game -> [Point]
+getIdlePoints game = 
+    let 
+        b@(Board w h) = board game
+        s = snake game
+        allPoints = [Point x y | x <- [0..w-1], y <- [0..h-1]]
+    in
+        allPoints \\ (getBoardPoints b ++ body s ++ [fruit game])
 
 instance Drawable Game where
     draw game = do
         -- clear manually
-        mapM_ (\(x, y) -> putPoint (toInteger x) (toInteger y) " ") $ allPoints
+        mapM_ (\(Point x y) -> putPoint (toInteger x) (toInteger y) " ") $ getIdlePoints game
         draw $ snake game
         draw $ board game
-        where 
-            w = width $ board game
-            h = height $ board game
-            allPoints = [(x, y) | x <- [0..w-1], y <- [0..h-1]]
+        draw $ fruit game
+
+genFruit :: Game -> IO (Fruit)
+genFruit game = randomRIO (0, length idles - 1) >>= return . (idles !!)
+    where idles = getIdlePoints game
 
 getCommand :: NC.Window -> NC.Curses (Maybe Direction)
 getCommand w = do
